8ec787355457867d3d9674e42bc1951d51ba3883
daldric <daldric1@binghamton.edu>
2025-10-28 20:43:15 -0400

    basic running server

---
+++ server.js
@@ -0,0 +1,25 @@
+// server.js
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+// Serve a simple HTML page
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'index.html');
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Error loading page');
+    } else {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`✅ Server running at http://localhost:${PORT}`);
+});
